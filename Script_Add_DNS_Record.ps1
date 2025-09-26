6faaf53ef96a6e8be764e6b16336a4523b5bc316
Jeremy Rousseau <62570461+jeremy9336@users.noreply.github.com>
2025-09-26 16:56:37 -0500

    Add files via upload

---
+++ Script_Add_DNS_Record.ps1
@@ -0,0 +1,42 @@
+#+-------------------------------------------------------------+  
+#| Author:  Jeremy Rousseau
+#| Purpose: Add DNS Record
+#| Version: 1.0
+#+--------------------------------------------------------------
+
+#+-------------------------------------------------------------+  
+#| Change Log:
+#| Version 1.0 - Initial script
+#+-------------------------------------------------------------+
+
+# Environment Setup 
+$DNSServer = "SET DNS SERVER" 
+$DNSZone = "SET DNS ZONE" 
+$InputFile = "dnsadd.csv" 
+ 
+# Read the input file which is formatted as name,type,address with a header row 
+$records = Import-CSV $InputFile 
+ 
+# Now we loop through the file to delete and re-create records 
+# DNSCMD does not have a modify option so we must use /RecordDelete first followed by a /RecordAdd  
+ 
+ForEach ($record in $records) { 
+ 
+    # Capture the record contents as variables 
+    $recordName = $record.name 
+    $recordType = $record.type 
+    $recordAddress = $record.address 
+ 
+    # Build our DNSCMD DELETE command syntax 
+    $cmdDelete = "dnscmd $DNSServer /RecordDelete $DNSZone $recordName $recordType /f" 
+ 
+    # Build our DNSCMD ADD command syntax 
+    $cmdAdd = "dnscmd $DNSServer /RecordAdd $DNSZone $recordName $recordType $recordAddress" 
+ 
+    # Now we execute the command 
+    #Write-Host "Running the following command: $cmdDelete" 
+    #Invoke-Expression $cmdDelete 
+ 
+    Write-Host "Running the following command: $cmdAdd" 
+    Invoke-Expression $cmdAdd 
+}
